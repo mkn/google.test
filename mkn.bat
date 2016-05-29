a7ec79116b839245414a02854eebe655e85a72e1
Dekken <philip.deegan@gmail.com>
2016-05-29 21:51:59 +0100

    Update mkn.bat

--- mkn.bat
+++ mkn.bat
@@ -2,26 +2,32 @@
 
 @ECHO off
 
-rd /S/Q g
-rd /S/Q google
+IF EXIST %PWD%\g (
+	rd /S/Q  %CD%\g
+)
+
+IF EXIST %CD%\google (
+	rd /S/Q %CD%\google
+)
+
 git clone https://github.com/google/googletest -b master g --recursive || exit 666 /b
 
-mkdir google
-mkdir google\test
-mkdir google\mock
+mkdir %CD%\google
+mkdir %CD%\google\test
+mkdir %CD%\google\mock
 
+ROBOCOPY %CD%\g\googletest %CD%\google\test /NFL /NDL /NJH /NJS /nc /ns /np
 ROBOCOPY /s %CD%\g\googletest\include %CD%\google\test\include /NFL /NDL /NJH /NJS /nc /ns /np
 ROBOCOPY /s %CD%\g\googletest\src %CD%\google\test\src /NFL /NDL /NJH /NJS /nc /ns /np
-ROBOCOPY %CD%\g\googletest\* %CD%\google\test /NFL /NDL /NJH /NJS /nc /ns /np
-DEL google\test\Ma*
-DEL google\test\CM*
-DEL google\test\co*
+DEL /Q %CD%\google\test\Ma*
+DEL /Q %CD%\google\test\CM*
+DEL /Q %CD%\google\test\co*
 
+ROBOCOPY %CD%\g\googlemock %CD%\google\mock /NFL /NDL /NJH /NJS /nc /ns /np
 ROBOCOPY /s %CD%\g\googlemock\include %CD%\google\mock\include /NFL /NDL /NJH /NJS /nc /ns /np
 ROBOCOPY /s %CD%\g\googlemock\src %CD%\google\mock\src /NFL /NDL /NJH /NJS /nc /ns /np
-ROBOCOPY %CD%\g\googlemock\* %CD%\google\mock /NFL /NDL /NJH /NJS /nc /ns /np
-DEL google\mock\Ma*
-DEL google\mock\CM*
-DEL google\mock\co*
+DEL /Q %CD%\google\mock\Ma*
+DEL /Q %CD%\google\mock\CM*
+DEL /Q %CD%\google\mock\co*
 
 rd /S/Q g
