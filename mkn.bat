1ead0ca2871f4a4b0951667597c64f0e3fbbdfb9
dekken <philip.deegan@gmail.com>
2022-08-22 23:11:40 +0200

    use last c++11 version

--- mkn.bat
+++ mkn.bat
@@ -5,4 +5,4 @@ IF EXIST %PWD%\g (
 	rd /S/Q  %CD%\g
 )
 
-git clone --depth 1 https://github.com/google/googletest g --recursive || exit 666 /b
+git clone --depth 1 https://github.com/google/googletest g --recursive -b release-1.12.1 || exit 666 /b
--- mkn.sh
+++ mkn.sh
@@ -1,4 +1,4 @@
 
 rm -rf g
 
-git clone --depth 1 https://github.com/google/googletest --recursive g
+git clone --depth 1 https://github.com/google/googletest --recursive g -b release-1.12.1
